13428560840d00b4b9236a8e6a30bf55e45580bb
Artem Solopiy <artem.solopiy.gdc@ts.fujitsu.com>
2016-06-20 14:43:00 +0300

    * Missing files added
    * Db project added

---
+++ EntityFX.EconomicsArcade.Infrastructure.Service/InfrastructureProxyFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFX.EconomicsArcade.Infrastructure.Service
+{
+    public abstract class InfrastructureProxyFactory<TServiceContract> : IDisposable
+    {
+        private TServiceContract _clientProxy;
+        private OperationContextScope _operationContextScope;
+        private ChannelFactory<TServiceContract> _channelFactory;
+
+        protected virtual TServiceContract CreateProxy(Uri endpointAddress)
+        {
+            var binding = GetBinding();
+            _channelFactory = new ChannelFactory<TServiceContract>(binding, new EndpointAddress(endpointAddress));
+            return _channelFactory.CreateChannel();
+        }
+
+        protected abstract Binding GetBinding();
+
+        protected OperationContextScope CreateContextScope()
+        {
+            return new OperationContextScope((IContextChannel)_clientProxy);
+        }
+
+        protected virtual void ApplyOperationContext()
+        {
+
+        }
+
+        public TServiceContract OpenChannel(Uri endpointAddress)
+        {
+            _clientProxy = CreateProxy(endpointAddress);
+            _operationContextScope = CreateContextScope();
+            ApplyOperationContext();
+            return _clientProxy;
+        }
+
+        public void CloseChannel()
+        {
+            if (_clientProxy != null) ((IClientChannel)_clientProxy).Close();
+            if (_channelFactory != null) _channelFactory.Close();
+        }
+
+        public void Dispose()
+        {
+            if (_clientProxy != null) _operationContextScope.Dispose();
+            if (_clientProxy != null) ((IDisposable)_clientProxy).Dispose();
+            if (_channelFactory != null) ((IDisposable)_channelFactory).Dispose();
+        }
+    }
+}
