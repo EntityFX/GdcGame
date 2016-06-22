a23619d0b40c39e29985a01d91ff2b4a42c49d7f
Artem Solopiy <artem.solopiy.gdc@ts.fujitsu.com>
2016-06-22 17:14:54 +0300

    Incrementors data sql added

---
+++ EntityFX.EconomicsArcade.Database/Data/DataIncrementors.sql
@@ -0,0 +1,46 @@
+﻿INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (0, 0, CAST(1 AS Decimal(18, 0)), 1, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (1, 0, CAST(10 AS Decimal(18, 0)), 1, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (2, 0, CAST(1 AS Decimal(18, 0)), 2, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (3, 0, CAST(10 AS Decimal(18, 0)), 2, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (4, 0, CAST(2 AS Decimal(18, 0)), 3, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (5, 0, CAST(20 AS Decimal(18, 0)), 3, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (6, 0, CAST(20 AS Decimal(18, 0)), 3, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (7, 0, CAST(80 AS Decimal(18, 0)), 4, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (8, 0, CAST(40 AS Decimal(18, 0)), 4, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (9, 0, CAST(160 AS Decimal(18, 0)), 5, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (10, 0, CAST(10 AS Decimal(18, 0)), 6, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (11, 0, CAST(80 AS Decimal(18, 0)), 6, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (12, 0, CAST(5 AS Decimal(18, 0)), 7, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (13, 0, CAST(240 AS Decimal(18, 0)), 7, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (14, 0, CAST(80 AS Decimal(18, 0)), 7, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (15, 0, CAST(5 AS Decimal(18, 0)), 8, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (16, 0, CAST(80 AS Decimal(18, 0)), 8, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (17, 0, CAST(80 AS Decimal(18, 0)), 8, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (18, 0, CAST(10 AS Decimal(18, 0)), 9, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (19, 1, CAST(2 AS Decimal(18, 0)), 9, 1)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (20, 0, CAST(80 AS Decimal(18, 0)), 9, 2)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (21, 0, CAST(10 AS Decimal(18, 0)), 10, 0)
+
+INSERT [dbo].[Incrementor] ([Id], [Type], [Value], [FundsDriverId], [CounterId]) VALUES (22, 0, CAST(150 AS Decimal(18, 0)), 10, 2)
+
