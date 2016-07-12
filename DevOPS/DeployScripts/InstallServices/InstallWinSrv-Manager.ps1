7a119feb4e61fa1cfa5c4a0f4f4b3481ffebd161
Niyaz Mubarakzyanov <Niyaz.Mubarakzyanov@icl-services.com>
2016-07-12 14:37:37 +0300

    Add data sql script and fix installation scripts

--- DevOPS/DeployScripts/InstallServices/InstallWinSrv-Manager.ps1
+++ DevOPS/DeployScripts/InstallServices/InstallWinSrv-Manager.ps1
@@ -2,6 +2,6 @@ $exeFile = "EntityFX.EconomicsArcade.Utils.ServiceHost.Manager.WindowsSrv.exe"
 $installPath = "C:\Users\Administrator\Desktop\AppServer"
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe $installPath\$exeFile
-net start "EntityFX.EconomicsArcade.Utils.ServiceHost.DataAccess.WindowsSrv"
+net start "EntityFX.EconomicsArcade.Utils.ServiceHost.Manager.WindowsSrv"
 
 Read-Host "Press Enter to continue"
--- DevOPS/DeployScripts/InstallServices/InstallWinSrv-NotifyConsumer.ps1
+++ DevOPS/DeployScripts/InstallServices/InstallWinSrv-NotifyConsumer.ps1
@@ -2,6 +2,6 @@ $exeFile = "EntityFX.EconomicsArcade.Utils.ServiceHost.NotifyConsumer.WindowsSrv
 $installPath = "C:\Users\Administrator\Desktop\"
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe $installPath\$exeFile
-net start "EntityFX.EconomicsArcade.Utils.ServiceHost.DataAccess.WindowsSrv"
+net start "EntityFX.EconomicsArcade.Utils.ServiceHost.NotifyConsumer.WindowsSrv"
 
 Read-Host "Press Enter to continue"
