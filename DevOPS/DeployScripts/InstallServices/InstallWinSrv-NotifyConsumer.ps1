106cc09bc226e78770d96e783215920268d09101
Niyaz Mubarakzyanov <Niyaz.Mubarakzyanov@icl-services.com>
2016-07-12 15:38:06 +0300

    Add webPublish profile and fix installing scripts

--- DevOPS/DeployScripts/InstallServices/InstallWinSrv-NotifyConsumer.ps1
+++ DevOPS/DeployScripts/InstallServices/InstallWinSrv-NotifyConsumer.ps1
@@ -1,5 +1,5 @@
 $exeFile = "EntityFX.EconomicsArcade.Utils.ServiceHost.NotifyConsumer.WindowsSrv.exe"
-$installPath = "C:\Users\Administrator\Desktop\"
+$installPath = "C:\Users\Administrator\Desktop\NotifyConsumer"
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe $installPath\$exeFile
 net start "EntityFX.EconomicsArcade.Utils.ServiceHost.NotifyConsumer.WindowsSrv"
