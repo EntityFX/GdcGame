22622b699e51a49fcb9bb02f8af2dd6b10fdf596
EntityFX <artem.solopiy@gmail.com>
2016-08-23 20:29:40 +0300

    Added missing file

---
+++ Sources/EntityFX.Gdcame.Utils.ServiceStarter.Collapsed/NotifyConsumerServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFX.Gdcame.Common.Contract;
+using EntityFX.Gdcame.DataAccess.Contract.GameData;
+using EntityFX.Gdcame.Infrastructure.Service.Bases;
+using EntityFX.Gdcame.NotifyConsumer.Contract;
+
+namespace EntityFX.Gdcame.Utils.ServiceStarter.Collapsed
+{
+    internal class NotifyConsumerServiceClient<TInfrastructureProxy> : INotifyConsumerService
+        where TInfrastructureProxy : IInfrastructureProxy<INotifyConsumerService, Binding>, new()
+    {
+        public void PushGameData(UserContext userContext, GameData gameData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
