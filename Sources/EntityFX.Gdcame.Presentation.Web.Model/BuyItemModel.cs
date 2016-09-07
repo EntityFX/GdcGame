6ece1b39c7be129db14a52bf3276a88e30a4410f
EntityFX <artem.solopiy@gmail.com>
2016-09-07 12:52:15 +0300

    Added missing files

---
+++ Sources/EntityFX.Gdcame.Presentation.Web.Model/BuyItemModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityFX.Gdcame.Common.Presentation.Model;
+
+namespace EntityFX.Gdcame.Presentation.Contract.Model
+{
+    public class BuyItemModel
+    {
+        public ItemModel ItemBuyInfo { get; set; }
+
+        public CashModel ModifiedCountersInfo { get; set; }
+    }
+}
