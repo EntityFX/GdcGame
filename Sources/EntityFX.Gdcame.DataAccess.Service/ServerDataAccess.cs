406a479afcc0ee5825ea5532e40f750e927ff007
EntityFX <artem.solopiy@gmail.com>
2016-10-19 21:56:16 +0300

    Added server manager

---
+++ Sources/EntityFX.Gdcame.DataAccess.Service/ServerDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using EntityFX.Gdcame.DataAccess.Contract.Server;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFX.Gdcame.DataAccess.Service
+{
+    public class ServerDataAccessService : IServerDataAccessService
+    {
+        public string[] GetServers()
+        {
+            if (!File.Exists(Path.Combine("servers.json")))
+            {
+                return null;
+            }
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText(Path.Combine("servers.json")))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.TypeNameHandling = TypeNameHandling.Auto;
+                return (string[])serializer.Deserialize(file, typeof(string[]));
+            }
+        }
+    }
+}
