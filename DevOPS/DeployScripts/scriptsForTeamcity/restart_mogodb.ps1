5b7f0e3edabf2cd1eccfccc301be4b5403204c33
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2016-11-23 19:46:11 +0300

    Updated

--- DevOPS/DeployScripts/scriptsForTeamcity/restart_mogodb.ps1
+++ DevOPS/DeployScripts/scriptsForTeamcity/restart_mogodb.ps1
@@ -3,16 +3,13 @@ $Password = 'P@ssw0rd'
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 
-$binsVariableName = 'GDCame_Bins'
 $TEAMCITY_HOME = $env:TEAMCITY_HOME
+$scriptPath = echo $env:GDCame_Bins'\cmds\restart_mongodb.cmd'
+$scriptBlock = [Scriptblock]::Create($scriptPath)
 
 $serverlist = Get-Content $TEAMCITY_HOME\scripts\server_list.txt
 
 foreach ($server in $serverlist) {
-
-	$remoteBinsPath = @(gwmi win32_environment -computername $server | Where-Object {$_.Name -eq $binsVariableName} | select VariableValue).VariableValue
-	$importScriptPath = echo $remoteBinsPath'\mongo-scripts\restart_mongodb.cmd'
-	$scriptBlock = [Scriptblock]::Create($importScriptPath)
 
 	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {$server}
 }
