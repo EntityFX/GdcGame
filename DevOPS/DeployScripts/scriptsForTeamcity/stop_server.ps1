d797041bd6106ee8176dfdc3497737202de014e9
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2016-11-24 14:02:03 +0300

    Revert "Changed script"
    This reverts commit 510c96c65967e086f5c5715d5b793c238fa27401.

--- DevOPS/DeployScripts/scriptsForTeamcity/stop_server.ps1
+++ DevOPS/DeployScripts/scriptsForTeamcity/stop_server.ps1
@@ -4,15 +4,12 @@ $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 
 $TEAMCITY_HOME = $env:TEAMCITY_HOME
-#$scriptPath = echo $env:GDCame_Bins'\cmds\stop_server.cmd'
-$scriptPath = echo $env:GDCame_Bins'\EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe'
+$scriptPath = echo $env:GDCame_Bins'\cmds\stop_server.cmd'
 $scriptBlock = [Scriptblock]::Create($scriptPath)
 
 $serverlist = Get-Content $TEAMCITY_HOME\scripts\server_list.txt
 
 foreach ($server in $serverlist) {
 
-	#Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {$server}
-	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {"stop"}
-	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {"uninstall"}
+	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {$server}
 }
