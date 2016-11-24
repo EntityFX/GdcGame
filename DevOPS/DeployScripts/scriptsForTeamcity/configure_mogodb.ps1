d310be229d48a01ce4183fb1f4d5247c1a8360f7
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2016-11-24 10:29:39 +0300

    deleted absolute path

--- DevOPS/DeployScripts/scriptsForTeamcity/configure_mogodb.ps1
+++ DevOPS/DeployScripts/scriptsForTeamcity/configure_mogodb.ps1
@@ -2,10 +2,6 @@ $TEAMCITY_HOME = $env:TEAMCITY_HOME
 
 $serverlist = Get-Content $TEAMCITY_HOME\scripts\server_list.txt
 
-$mongoScripts = 'C:\TeamCity\buildAgent\work\47b557396722e8a\bin\mongo-scripts'
-
-cd $mongoScripts
-
 foreach ($server in $serverlist) {
 	.\import.cmd $server
 }
