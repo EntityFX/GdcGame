510c96c65967e086f5c5715d5b793c238fa27401
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2016-11-24 13:11:32 +0300

    Changed script

--- DevOPS/DeployScripts/scriptsForTeamcity/stop_server.ps1
+++ DevOPS/DeployScripts/scriptsForTeamcity/stop_server.ps1
@@ -4,12 +4,15 @@ $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 
 $TEAMCITY_HOME = $env:TEAMCITY_HOME
-$scriptPath = echo $env:GDCame_Bins'\cmds\stop_server.cmd'
+#$scriptPath = echo $env:GDCame_Bins'\cmds\stop_server.cmd'
+$scriptPath = echo $env:GDCame_Bins'\EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe'
 $scriptBlock = [Scriptblock]::Create($scriptPath)
 
 $serverlist = Get-Content $TEAMCITY_HOME\scripts\server_list.txt
 
 foreach ($server in $serverlist) {
 
-	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {$server}
+	#Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {$server}
+	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {"stop"}
+	Invoke-command -Computername $server -Credential $Cred -scriptblock $scriptBlock -ArgumentList {"uninstall"}
 }
