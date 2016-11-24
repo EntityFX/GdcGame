74261de272b38c584ee150c6196b95b4cdedab9e
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2016-11-24 14:02:52 +0300

    Changed path

--- DevOPS/DeployScripts/scriptsForTeamcity/cmds/restart_server.cmd
+++ DevOPS/DeployScripts/scriptsForTeamcity/cmds/restart_server.cmd
@@ -1,3 +1,3 @@
-cd ..
+cd C:\SharedTest
 EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe install --autostart
 EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe start
--- DevOPS/DeployScripts/scriptsForTeamcity/cmds/stop_server.cmd
+++ DevOPS/DeployScripts/scriptsForTeamcity/cmds/stop_server.cmd
@@ -1,3 +1,3 @@
-cd ..
+cd C:\SharedTest
 EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe stop
 EntityFX.Gdcame.Utils.ConsoleHostApp.AllInOne.exe uninstall
