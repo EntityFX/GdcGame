4f1d95303275f4183f17883af2bf89bb14f77e05
SaubanovaR <Ruzilya.Saubanova.GDC@ts.fujitsu.com>
2017-05-14 13:59:38 +0300

    Added RendezvousHash and Md5HashFunction

---
+++ Sources/EntityFX.Gdcame.Utils.Shared/Rendezvous Hashing/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFX.Gdcame.Utils.Shared.Rendezvous_Hashing
+{
+    class Node
+    {
+        public Node()
+        {
+        }
+
+        public Node(string serverAddress, int serverNumber)
+        {
+            _serverAddress = serverAddress;
+            _serverNumber = serverNumber;
+        }
+
+        /**
+	     * Server address written in servers.json. Usually IP. Should be put in "http://{0}:{1}/" instead of {0}.
+         * See <link>EntityFX.Gdcame.Presentation.WebApiConsoleClient.ApiHelper.GetApiServerUri</link>
+	     */
+        private string _serverAddress;
+
+        public string ServerAddress
+        {
+            get { return _serverAddress; }
+            set { _serverAddress = value; }
+        }
+
+        private int _serverNumber;
+
+        public int ServerNumber
+        {
+            get { return _serverNumber; }
+            set { _serverNumber = value; }
+        }
+    }
+}
