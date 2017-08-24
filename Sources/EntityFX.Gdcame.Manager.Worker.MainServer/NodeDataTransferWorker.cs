0a99dbd7d11ba632599e109803e3676f30faf497
Alexander Utochkin <Alexander.Utochkin.GDC2@ts.fujitsu.com>
2017-08-24 17:46:51 +0300

    Get users for transfer

--- Sources/EntityFX.Gdcame.Manager.Worker.MainServer/NodeDataTransferWorker.cs
+++ Sources/EntityFX.Gdcame.Manager.Worker.MainServer/NodeDataTransferWorker.cs
@@ -9,6 +9,11 @@
     using EntityFX.Gdcame.Engine.Contract.GameEngine;
     using EntityFX.Gdcame.Manager.Contract.Common.WorkerManager;
     using DataAccess.Contract.MainServer.GameData;
+    using System.Linq;
+    using Infrastructure.Common;
+    using System.Collections.Generic;
+    using Kernel.Contract;
+    using DataAccess.Contract.MainServer.GameData.Store;
 
     public class NodeDataTransferWorker : WorkerBase
     {
@@ -16,7 +21,13 @@
 
         private readonly IUserDataAccessService userDataAccessService;
 
+        private readonly IGameDataRetrieveDataAccessService gameDataRetrieveDataAccessService;
+
         private readonly IGameSessions gameSessions;
+
+        private readonly IHashHelper hashHelper;
+
+        private readonly IMapper<IGame, StoredGameData> gameMapper;
 
         /// <summary>
         /// The _data transfer task.
@@ -24,12 +35,14 @@
         private Task _dataTransferTask;
         private readonly CancellationTokenSource cancellationTaskToken = new CancellationTokenSource();
 
-        public NodeDataTransferWorker(IServerDataAccessService serverDataAccessService, IUserDataAccessService userDataAccessService, IGameSessions gameSessions, IGameDataRetrieveDataAccessService gameDataRetrieveDataAccessService)
+        public NodeDataTransferWorker(IServerDataAccessService serverDataAccessService, IUserDataAccessService userDataAccessService, IGameSessions gameSessions, IGameDataRetrieveDataAccessService gameDataRetrieveDataAccessService, IHashHelper hashHelper, IMapper<IGame, StoredGameData> gameMapper)
         {
             this.serverDataAccessService = serverDataAccessService;
             this.userDataAccessService = userDataAccessService;
             this.gameSessions = gameSessions;
             this.gameDataRetrieveDataAccessService = gameDataRetrieveDataAccessService;
+            this.hashHelper = hashHelper;
+            this.gameMapper = gameMapper;
             this.Name = "NodeDataTransferWorker";
         }
 
@@ -57,11 +70,60 @@
         private void PerformBackgroundPersisting(string[] servers)
         {
             this.IncrementTick();
-            foreach (var gamesKey in this.gameSessions.Games.Keys)
+
+            serverDataAccessService.UpdateServers(servers);
+
+            //foreach (var gamesKey in this.gameSessions.Games.Keys)
+            //{
+            //    this.gameSessions.FreezeUserGame(gamesKey, "127.0.0.1");
+            //}
+
+            int offset = 0;
+            int size = 100;
+
+            Dictionary<string, List<User>> serversDataDictionary = new Dictionary<string, List<User>>();
+
+            for (int i = 0; i < servers.Length; i++)
             {
-                this.gameSessions.FreezeUserGame(gamesKey, "127.0.0.1");
+                serversDataDictionary.Add(servers[i], new List<User>());
             }
-            //_serverDataAccessService.UpdateServers(newServersList);
+
+            while (true)
+            {
+                var users = userDataAccessService.FindChunked(offset, size);
+                if (users.Count() == 0) break;
+                foreach (var user in users)
+                {
+                    int newServerId = hashHelper.GetServerNumberByUserId(servers, user.Id);
+                    serversDataDictionary[servers[newServerId]].Add(user);
+                }
+                offset += 100;
+                List<User> usersForSend = new List<User>();
+                foreach (var server in serversDataDictionary)
+                {
+                    if (server.Key == "127.0.0.1") continue;
+                    foreach (var user in server.Value)
+                    {
+                        this.gameSessions.FreezeUserGame(user.Login, server.Key);
+                        
+                    }
+                    usersForSend.AddRange(server.Value);        //users that should be sent to new servers
+                }
+
+                var usersSavedData = this.gameDataRetrieveDataAccessService.GetStoredGameData(usersForSend.Select(u => u.Id).ToArray());        //saved games for users for send
+
+                foreach (var game in gameSessions.Games)        //update saved games data with active games
+                {
+                    var activeUser = usersForSend.Where(user => user.Login == game.Key).SingleOrDefault();
+                    if(activeUser!=null)
+                    {
+                        var newStoredDataForUser = gameMapper.Map(game.Value);      // IGame - > StoredGameData
+                        usersSavedData.Where(data => data.UserId == activeUser.Id).SingleOrDefault().StoredGameData = newStoredDataForUser;     //replace old saved data to new data
+                    }
+                }
+                //send data to servers
+            }
+            
         }
 
         public override bool IsRunOnStart
